c00282f1b2250868d7918520abb3d039a8ab5a69
dario <reco@gmail.com>
2022-02-11 17:22:04 +0100

    C:Usersreco_OneDrivevoluntariogit_DarioyMiguel

--- RamasGit/ClasePruebaGIT.java
+++ RamasGit/ClasePruebaGIT.java
@@ -2,6 +2,6 @@ package RamasGit;
 
 public class ClasePruebaGIT {
 	public static void main(String[] ags) {
-	System.out.println("Di hola a la camara");
+	System.out.println("Di hola a la cadddddddddddddmara");
 	}
 }
